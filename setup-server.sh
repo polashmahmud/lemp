79943ccbc95f7b5eff145a4428e2fca468e7dff2
Polash Mahmud <polashmahmud@gmail.com>
2024-09-16 09:59:07 +0600

    update bash file

--- setup-server.sh
+++ setup-server.sh
@@ -6,6 +6,6 @@ if dpkg-query -W needrestart >/dev/null 2>&1; then
     sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
 fi
 
-echo 'Acquire::ForceIPv4 "true";' | tee /etc/apt/apt.conf.d/99force-ipv4
+echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
 sudo apt update -y
 sudo apt upgrade -y
